2f8d96dd1298c0a86b561869ef1c107e6ad7250e
Floréale BARRAUD <floreale.barraud@edu.devinci.fr>
2020-10-07 18:28:03 +0300

    Add project files.

---
+++ POP - class 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POP___class_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
